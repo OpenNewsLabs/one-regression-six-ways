93df3dfb67ebe6b98a236601cfb917e59797ddae
Christine Zhang <christineyiwen.zhang@gmail.com>
2016-06-12 17:31:55 -0700

    first commit

---
+++ R/regression.R
@@ -0,0 +1,17 @@
+# set working directory
+setwd("/Users/christinezhang/Projects/regression/R")
+
+# read data
+d <- read.csv("data.csv")
+
+# run regression
+reg <- lm(health ~ log(income), data = d)
+summary(reg)
+
+# assess the regression model
+
+png('plots.png', width = 8, height = 7, units = 'in', res = 200)
+par(mfrow = c(2,2))
+plot(reg)
+dev.off()
+
