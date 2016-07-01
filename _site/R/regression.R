2c34b31f62520d8d82bf5c2bc44fd4f0af54ba0b
Christine Zhang <christineyiwen.zhang@gmail.com>
2016-07-01 11:23:32 -0700

    regression

---
+++ _site/R/regression.R
@@ -0,0 +1,26 @@
+# set working directory - modify as necessary
+# setwd("/Users/christinezhang/Projects/regression/R")
+
+# read data
+d <- read.csv("data.csv", stringsAsFactors = F)
+
+# run regression
+reg <- lm(health ~ log(income), data = d)
+summary(reg)
+
+# assess the regression model
+png('regplots.png', width = 8, height = 7, units = 'in', res = 200)
+par(mfrow = c(2,2))
+plot(reg)
+dev.off()
+
+# put residuals (raw & standardized) plus fitted values into a data frame
+results <- cbind.data.frame(country = d$country,
+                               resid = resid(reg), 
+                               std_resids = rstandard(reg), 
+                               stu_resids = rstudent(reg), 
+                               fitted = fitted(reg))
+
+head(results)
+
+
